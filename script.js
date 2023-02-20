51422ee27d4886a01317125590c3fa0569d21bb3
Sebastián Garrido <sebgarridor@gmail.com>
2023-02-20 17:23:59 -0300

    adding smooth scrolling

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-//smooth scrolling
-const body = document.body,
-scrollWrap = document.getElementsByClassName("container")[0],
-height = scrollWrap.getBoundingClientRect().height - 1,
-speed = 0.04;
-
-var offset = 0;
-
-body.style.height = Math.floor(height) + "px";
-
-function smoothScroll() {
-offset += (window.pageYOffset - offset) * speed;
-
-var scroll = "translateY(-" + offset + "px) translateZ(0)";
-scrollWrap.style.transform = scroll;
-
-callScroll = requestAnimationFrame(smoothScroll);
-}
-
-smoothScroll();
-
 
 //hover text effect
 
